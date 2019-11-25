9a2e470a17292172b313c8eb0ae772b21e313217
John Fantell <fantej@rpi.edu>
2019-11-25 06:35:49 -0500

    Work in Progress- Abstract models away from drawing and vice versa -- shadows should take into account all objects

---
+++ shadows/src/draw.cpp
@@ -0,0 +1,5 @@
+//
+// Created by John Fantell on 11/25/19.
+//
+
+#include "draw.h"
---
+++ shadows/src/draw.h
@@ -0,0 +1,8 @@
+//
+// Created by John Fantell on 11/25/19.
+//
+
+#ifndef SHADOWS_DRAW_H
+#define SHADOWS_DRAW_H
+
+#endif //SHADOWS_DRAW_H
